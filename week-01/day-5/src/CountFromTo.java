2eb1b4c00e051f91b07c54199a63efa728a9efc1
Gabor Horvath <ghorvath84@gmail.com>
2019-09-13 11:01:20 +0200

    Added CountFromTo.java

---
+++ week-01/day-5/src/CountFromTo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CountFromTo {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please give 2 number to count:");
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        if (b <= a) {
+            System.out.println("The second number should be bigger!");
+        } else {
+            for (int i = a; i <= b; i++) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
