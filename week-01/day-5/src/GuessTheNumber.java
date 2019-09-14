22c894cfd01fd0de9b7eb4da64d8015637dd8715
Gabor Horvath <ghorvath84@gmail.com>
2019-09-14 23:29:09 +0200

    Added GuessTheNumber.java

---
+++ week-01/day-5/src/GuessTheNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+    int mysteryNumber = 8;
+    int userTry;
+    System.out.print("Try to figure out the secret number, give me a number: ");
+
+    do {
+      userTry = input.nextInt();
+      if (userTry > mysteryNumber){
+        System.out.println("The stored number is lower");
+      } else if (userTry < mysteryNumber) {
+        System.out.println("The stored number is higher");
+      }
+    } while ( userTry != mysteryNumber);
+    System.out.println("You found the number: " + mysteryNumber);
+  }
+}
