5716c23f3992afadf27117bc957c2fb46fda07e3
Gabor Horvath <ghorvath84@gmail.com>
2019-09-15 00:10:55 +0200

    Added DrawChessTable.java

---
+++ week-01/day-5/src/DrawChessTable.java
@@ -0,0 +1,18 @@
+public class DrawChessTable {
+
+  public static void main(String[] args) {
+
+    for (int i = 1; i <= 8; i++) {
+      for (int j = 1; j <= 8; j++) {
+        if (i % 2 != 0 && j % 2 != 0) {
+          System.out.print("%");
+          System.out.print(" ");
+        } else if (i % 2 == 0 && j % 2 == 0) {
+          System.out.print(" ");
+          System.out.print("%");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
