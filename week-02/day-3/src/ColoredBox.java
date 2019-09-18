ec9d15bfeb75e41bd97db53ae106de66048bd96d
Gabor Horvath <ghorvath84@gmail.com>
2019-09-18 11:10:47 +0200

    Fixed LineInTheMiddle and Added ColoredBox.java

--- week-02/day-3/src/ColoredBox.java
+++ week-02/day-3/src/ColoredBox.java
@@ -1,24 +1,32 @@
 import javax.swing.*;
 
 import java.awt.*;
+import sun.plugin2.util.ColorUtil;
 
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class LineInTheMiddle {
+public class ColoredBox {
   public static void mainDraw(Graphics graphics) {
-    // draw a red horizontal line to the canvas' middle.
-    // draw a green vertical line to the canvas' middle.
-    graphics.setColor(Color.RED);
-    graphics.drawLine(0, HEIGHT / 2, WIDTH / 2, HEIGHT / 2);
-    graphics.setColor(Color.GREEN);
-    graphics.drawLine(WIDTH / 2, 0, WIDTH / 2, HEIGHT / 2);
+    // Draw a box that has different colored lines on each edge.
+    graphics.setColor(Color.BLUE);
+    graphics.drawRect(25, 25, 300, 300);
+
+    graphics.setColor(new Color(176, 40, 40));
+    graphics.drawLine(25, 25, 325, 25);
+    graphics.setColor(new Color(118, 95, 24));
+    graphics.drawLine(25, 325, 325, 325);
+    graphics.setColor(new Color(184, 23, 88));
+    graphics.drawLine(25, 25, 25, 325);
+    graphics.setColor(new Color(62, 146, 19));
+    graphics.drawLine(325, 25, 325, 325);
+
 
   }
 
   // Don't touch the code below
-  static int WIDTH = 640;
-  static int HEIGHT = 480;
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
