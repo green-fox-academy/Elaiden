cc70d3f96a557c76623757ab5f9314ca87a66df3
Gabor Horvath <ghorvath84@gmail.com>
2019-09-19 17:31:50 +0200

    Fixed LinePlay Part1

--- week-02/day-4/src/LinePlay.java
+++ week-02/day-4/src/LinePlay.java
@@ -10,22 +10,24 @@ public class LinePlay {
 
     // Project Line Play
 
-    int steps = WIDTH / 16;
+    int size = 16;
+    int steps = WIDTH / size;
     int x1 = 0;
     int y1 = 0;
-    int x2 = WIDTH - (WIDTH - steps);
+    int x2 = steps;
     int y2 = HEIGHT;
-    for (int i = 0; i < steps; i++) {
+    for (int i = 0; i < size; i++) {
       graphics.setColor(Color.GREEN);
       graphics.drawLine(x1, y1, x2, y2);
       y1 += steps;
       x2 += steps;
     }
+    steps = HEIGHT / size;
     x1 = 0;
     y1 = 0;
     x2 = WIDTH;
-    y2 = HEIGHT - (HEIGHT - steps);
-    for (int j = 0; j < steps; j++) {
+    y2 = 0;
+    for (int j = 0; j < size; j++) {
       graphics.setColor(Color.BLUE);
       graphics.drawLine(x1, y1, x2, y2);
       x1 += steps;
