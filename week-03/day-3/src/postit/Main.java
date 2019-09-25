c3cef48a4242c8bf94aab81047694958905b013e
Gabor Horvath <ghorvath84@gmail.com>
2019-09-25 14:45:42 +0200

    Added the missing task descriptions

--- week-03/day-3/src/postit/Main.java
+++ week-03/day-3/src/postit/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
   public static void main(String[] args) {
 
+    /*Create a PostIt class that has
+        a backgroundColor
+        a text on it
+        a textColor
+        Create a few example post-it objects:
+    an orange with blue text: "Idea 1"
+    a pink with black text: "Awesome"
+    a yellow with green text: "Superb!"*/
+
     PostIt firstPost = new PostIt(Color.ORANGE, "Idea 1", Color.BLUE);
     PostIt secondPost = new PostIt(Color.PINK, "Awesome", Color.BLACK);
     PostIt thirdPost = new PostIt(Color.YELLOW, "Superb!", Color.GREEN);
