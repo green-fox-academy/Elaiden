20bb0521ea372e91169442e8fe7af1f0bedef342
Gabor Horvath <ghorvath84@gmail.com>
2019-10-01 12:50:29 +0200

    Added Cloneable

---
+++ week-04/day-2/src/cloneable/Student.java
@@ -0,0 +1,39 @@
+package cloneable;
+
+public class Student extends Person {
+
+  private String previousOrganization;
+  private int skippedDays;
+
+  public Student() {
+    super();
+    this.setPreviousOrganization("The School of Life");
+    this.setSkippedDays(0);
+  }
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.setPreviousOrganization(previousOrganization);
+    this.setSkippedDays(0);
+  }
+
+  @Override
+  protected Object clone() {
+    return this;
+  }
+
+  public void setPreviousOrganization(String previousOrganization) {
+    this.previousOrganization = previousOrganization;
+  }
+
+  public void setSkippedDays(int skippedDays) {
+    this.skippedDays = skippedDays;
+  }
+
+  public void introduce() {
+    System.out.println(
+        "Hi, I'm " + this.getName() + ", a " + this.getAge() + " year old " + this.getGender()
+            + " from " + previousOrganization
+            + " who skipped " + skippedDays + " days from the course already.");
+  }
+}
