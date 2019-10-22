ce531f44bc94a91bc049b9b27fb41c3984140aa4
Elaiden <ghorvath84@gmail.com>
2019-10-22 14:43:11 +0200

    Added REST greet counter app

--- week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -2,19 +2,11 @@ package com.greenfoxacademy.springstart.controllers;
 
 public class Greeting {
 
-  private long id;
-  private String content;
+  // private long id;
+  // private String content;
 
-  public Greeting(long id, String content) {
+  /*public Greeting(long id, String content) {
     this.id = id;
     this.content = content;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public String getContent() {
-    return content;
-  }
+  }*/
 }
--- week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,14 +1,18 @@
 package com.greenfoxacademy.springstart.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
-  @GetMapping(value = "/greeting")
-  public Greeting greeting(@RequestParam String name) {
-    return new Greeting(1, "Hello, " + name);
+  private AtomicLong greetCount = new AtomicLong();
+
+  @RequestMapping(value = "/greeting")
+  public String greeting(@RequestParam String name) {
+    greetCount.incrementAndGet();
+    return "Hello, " + name + ", the site has been refreshed " + greetCount + " times.";
   }
 }
