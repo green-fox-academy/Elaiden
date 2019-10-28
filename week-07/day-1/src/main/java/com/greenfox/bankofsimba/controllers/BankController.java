0167ccac9ad23a29943f52cfa7f13ae79130df7c
Elaiden <ghorvath84@gmail.com>
2019-10-28 14:23:23 +0100

    Finished tasks Filling multiple accounts and Extending more

--- week-07/day-1/src/main/java/com/greenfox/bankofsimba/controllers/BankController.java
+++ week-07/day-1/src/main/java/com/greenfox/bankofsimba/controllers/BankController.java
@@ -1,6 +1,8 @@
 package com.greenfox.bankofsimba.controllers;
 
 import com.greenfox.bankofsimba.models.BankAccount;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BankController {
 
+  private List<BankAccount> accounts = new ArrayList<>();
+
   @GetMapping(value = "/show")
   public String showAccount(Model model) {
     BankAccount simba = new BankAccount("Simba", 2000, "lion");
     model.addAttribute("account", simba);
-    return "account";
+    return "showAccounts";
   }
 
   @GetMapping(value = "/motivate")
@@ -21,4 +25,23 @@ public class BankController {
     return "motivate";
   }
 
+  @GetMapping(value = "/accounts")
+  public String showAccounts(Model model) {
+    accounts.add(new BankAccount("Simba", 2000, "lion"));
+    accounts.add(new BankAccount("Timon", 500, "meerkat"));
+    accounts.add(new BankAccount("Pumbaa", 1000, "warthog"));
+    accounts.add(new BankAccount("Scar", 20000, "lion"));
+    accounts.add(new BankAccount("Nala", 2, "lion"));
+    accounts.add(new BankAccount("Mufasa", 30000, "lion"));
+    model.addAttribute("accounts", accounts);
+    return "showAccounts";
+  }
+
+  public void setAccounts(List<BankAccount> accounts) {
+    this.accounts = accounts;
+  }
+
+  public List<BankAccount> getAccounts() {
+    return accounts;
+  }
 }
