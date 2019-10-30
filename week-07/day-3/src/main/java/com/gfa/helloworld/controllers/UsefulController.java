79b74212139da7087f8abfe09acf85c0c243d78f
Elaiden <ghorvath84@gmail.com>
2019-10-30 14:39:32 +0100

    Added Useful Utilities

---
+++ week-07/day-3/src/main/java/com/gfa/helloworld/controllers/UsefulController.java
@@ -0,0 +1,54 @@
+package com.gfa.helloworld.controllers;
+
+import com.gfa.helloworld.services.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UsefulController {
+
+  private UtilityService utilityService;
+
+  @Autowired
+  public UsefulController(UtilityService utilityService) {
+    this.utilityService = utilityService;
+  }
+
+  @GetMapping("/useful")
+  public String getContent(Model model) {
+    return "useful";
+  }
+
+  @GetMapping("/useful/colored")
+  public String coloredBackground(Model model) {
+    model.addAttribute("randomColor", utilityService.randomColor());
+    return "colored";
+  }
+
+  @GetMapping("/useful/email")
+  public String validateEmail(@RequestParam String email, Model model) {
+    if (utilityService.emailValidator(email)) {
+      model.addAttribute("emailColor", "green");
+    } else {
+      model.addAttribute("emailColor", "red");
+    }
+    model.addAttribute("email", email);
+    return "email";
+  }
+
+  @GetMapping("useful/encoding")
+  public String encodeText(@RequestParam String text, @RequestParam int key, Model model) {
+    model.addAttribute("encodedText", utilityService.caesar(text, key));
+    return "encoded";
+  }
+
+  @GetMapping("useful/decoding")
+  public String decodeText(@RequestParam String text, @RequestParam int key, Model model) {
+    int decodeKey = -key;
+    model.addAttribute("decodedText", utilityService.caesar(text, decodeKey));
+    return "decoded";
+  }
+}
